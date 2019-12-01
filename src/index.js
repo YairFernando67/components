834d6db1671ebf88ed233b597620473e813c27d1
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-01 11:51:46 -0600

    Using the props system to customize a commponent

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-        <CommentDetail />
-        <CommentDetail />
-        <CommentDetail />
-        <CommentDetail />
+      <CommentDetail
+        author="Yair"
+        timeAgo="Today at 6:00pm"
+        post="Nice job!!"
+        image={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Yesterday at 2:10am"
+        post="I liked it so much.."
+        image={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Laura"
+        timeAgo="Nov-12-2019 at 10:54pm"
+        post="Where are my frinds???"
+        image={faker.image.avatar()}
+      />
     </div>
-  )
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
