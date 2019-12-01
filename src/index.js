5ebcdbce5c6e9aadf44e16e8983b27167a543223
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-01 10:42:44 -0600

    Adding Semantic UI cdn to the project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi there</div>
+  return <h1>Hi there</h1>
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
