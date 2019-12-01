a086be47530515b5ddb342cd2a28ed2240e62890
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-01 12:55:14 -0600

    Using props system to work with parents and children components to pass data

--- src/index.js
+++ src/index.js
@@ -2,28 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Yair"
-        timeAgo="Today at 6:00pm"
-        post="Nice job!!"
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Yesterday at 2:10am"
-        post="I liked it so much.."
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Laura"
-        timeAgo="Nov-12-2019 at 10:54pm"
-        post="Where are my frinds???"
-        image={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Yair"
+          timeAgo="Today at 6:00pm"
+          post="Nice job!!"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Yesterday at 2:10am"
+          post="I liked it so much.."
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Laura"
+          timeAgo="Nov-12-2019 at 10:54pm"
+          post="Where are my frinds???"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
